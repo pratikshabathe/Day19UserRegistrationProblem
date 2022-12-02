4220c7f733db317144eecc56bc78084a20a6ec29
pratikshabathe <pratikshabathe11@gmail.com>
2022-12-02 06:22:31 +0530

    Create new files

--- Main.java
+++ Main.java
@@ -6,12 +6,18 @@ public class Main {
 	
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("--Welcom to user resistration problems--");
+		System.out.println("--Welcom to user resistration problem--");
 		checkFirstName();
+		checkLastName();
 	}
 		public static void checkFirstName() {
-			System.out.println("Enter first number: ");
+			System.out.println("Enter first name: ");
 			String fName = sc.next();
 			UserRegistration.userRegistration(fName);
+			}
+		public static void checkLastName() {
+			System.out.println("Enter Last name: ");
+			String lName = sc.next();
+			UserRegistration.userRegistration(lName);
 			}
 }
