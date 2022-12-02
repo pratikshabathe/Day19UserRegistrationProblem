d4008a2409c2c71d93cb9841c466bb06aa2f39f4
pratikshabathe <pratikshabathe11@gmail.com>
2022-12-02 13:30:09 +0530

    Added : create new files

--- Main.java
+++ Main.java
@@ -9,15 +9,21 @@ public class Main {
 		System.out.println("--Welcom to user resistration problem--");
 		checkFirstName();
 		checkLastName();
+		checkEmail();
 	}
 		public static void checkFirstName() {
 			System.out.println("Enter first name: ");
 			String fName = sc.next();
-			UserRegistration.userRegistration(fName);
+			UserRegistration.isValidName(fName);
 			}
 		public static void checkLastName() {
 			System.out.println("Enter Last name: ");
 			String lName = sc.next();
-			UserRegistration.userRegistration(lName);
+			UserRegistration.isValidName(lName);
+			}
+		public static void checkEmail() {
+			System.out.println("Enter an email: ");
+			String email = sc.next();
+			UserRegistration.isValidEmail(email);
 			}
 }
--- UserRegistration.java
+++ UserRegistration.java
@@ -5,15 +5,22 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
-	public static void userRegistration(String firstName) {
+	public static void isValidName(String name) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(firstName);
-		
+		Matcher m = p.matcher(name);		
+		if(m.matches())
+			System.out.println("valid");
+		else
+			System.out.println("Invalid");	
+	}
+	public static void isValidEmail(String email) {
+		String regex = "^[A-Za-z0-9.]+[@][a-z]{3,5}[.][a-z]{2,5}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(email);
 		if(m.matches())
 			System.out.println("valid");
 		else
 			System.out.println("Invalid");
-		
 	}
 }
