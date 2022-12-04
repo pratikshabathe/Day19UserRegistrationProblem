af086fdc9c068b72c6af23658ed024b3166c35e8
pratikshabathe <pratikshabathe11@gmail.com>
2022-12-04 18:30:06 +0530

    Added: create new files

--- Main.java
+++ Main.java
@@ -7,23 +7,27 @@ public class Main {
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("--Welcom to user resistration problem--");
-		checkFirstName();
-		checkLastName();
-		checkEmail();
-	}
-		public static void checkFirstName() {
+		switch(sc.nextInt()) {
+		case 1 :
 			System.out.println("Enter first name: ");
 			String fName = sc.next();
 			UserRegistration.isValidName(fName);
-			}
-		public static void checkLastName() {
+			
+		case 2 :
 			System.out.println("Enter Last name: ");
 			String lName = sc.next();
 			UserRegistration.isValidName(lName);
-			}
-		public static void checkEmail() {
+			
+		case 3:
 			System.out.println("Enter an email: ");
 			String email = sc.next();
 			UserRegistration.isValidEmail(email);
-			}
+		
+		case 4 :
+			System.out.println("Enter a phone number: ");
+			sc.nextLine();
+			String phNum = sc.nextLine();
+			UserRegistration.isValidEmail(phNum);
+		}
+	}
 }
--- UserRegistration.java
+++ UserRegistration.java
@@ -5,22 +5,35 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
-	public static void isValidName(String name) {
-		String regex = "^[A-Z]{1}[a-z]{2,}$";
+	private static boolean validate(String regex, String pattern) {
 		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(name);		
-		if(m.matches())
+		Matcher m = p.matcher(pattern);
+		return m.matches();
+	}
+	
+	public static void isValidName(String name) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";	
+		if(validate(regex, name))
 			System.out.println("valid");
 		else
 			System.out.println("Invalid");	
 	}
+	
 	public static void isValidEmail(String email) {
 		String regex = "^[A-Za-z0-9.]+[@][a-z]{3,5}[.][a-z]{2,5}$";
-		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(email);
-		if(m.matches())
+		if(validate(regex, email))
 			System.out.println("valid");
 		else
 			System.out.println("Invalid");
+	}
+	
+	public static void isValidPhoneNumber(String phNum) {
+		System.out.println(phNum);
+		//String regex = "^[+]\\d{2}?\\d{10}$";
+		String regex = "^[+][0-9]{1,3}(-[0-9]{1,3})?\\s{0,3}[6-9]{1}[0-9]{9}$";	
+		if(validate(regex, phNum))
+			System.out.println("valid");
+		else
+			System.out.println("Invalid");	
 	}
 }
